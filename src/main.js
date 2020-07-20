616637706eacf6f3bfc586ebb74cc99d53aaa349
Matan Greenvlad <mgk2798@gmail.com>
2020-07-20 16:00:50 +0300

    designed

--- src/main.js
+++ src/main.js
@@ -28,34 +28,54 @@ addButton.onclick = function(){
     div3.setAttribute("class", "todoPriority");
     div3.textContent = priority ;
 
-    let del = document.createElement("button");
-    del.textContent = "Delete";
-
-    let done = document.createElement("button");
-    done.textContent = "Done";
-
-    
-    // stracture in html
+    let hr = document.createElement("hr");
+    // stracture of line in html
     view.appendChild(container);
+    view.appendChild(hr);
     container.appendChild(div3);
     container.appendChild(div2);
     container.appendChild(div1);
+
+     // counter
+     let countdivs = document.getElementsByClassName("todoContainer").length;
+     let counter = document.querySelector("span");
+     counter.textContent = countdivs
+      
+    // delete button
+    let del = document.createElement("button");
+    del.setAttribute("id", "del");
+    del.textContent = "Delete";
+    del.onclick = function(){
+        view.removeChild(container);
+        view.removeChild(hr);
+        counter.textContent = document.getElementsByClassName("todoContainer").length;
+    }
+
+    // done button
+    let done = document.createElement("button");
+    done.setAttribute("id", "done")
+    done.textContent = "Done";
+    done.onclick = function(){
+        container.setAttribute("style", "text-decoration: line-through");
+        container.setAttribute("class", "finish")
+        counter.textContent = document.getElementsByClassName("todoContainer").length;
+    }
+
     container.appendChild(del);
     container.appendChild(done);
-    // counter
-    let countdivs = document.getElementsByClassName("todoContainer").length;
-    let counter = document.querySelector("span");
-    counter.textContent = countdivs
 
-    myArray.push();
-
+    
 
 };
 
+myArray.sort()
+       .reverse();
+let nodeList = document.querySelectorAll("div");
+let obj = {}
+
 
 //when click sort
-sortButton.onclick = function(){
-      
+sortButton.onclick = function(){  
 } 
 
 
