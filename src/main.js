8f9550edcbe4ae4324ccb53a0bde8877e8b1cfee
Matan Greenvlad <mgk2798@gmail.com>
2020-07-20 11:22:53 +0300

    css

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 // setting
+
 const addButton = document.getElementById("addButton");
+const sortButton = document.getElementById("sortButton");
 const input = document.querySelector("input");
 const select = document.querySelector("select");
 const view = document.querySelector("view");
+let myArray = [];
 // when click add
 addButton.onclick = function(){
     let task = input.value;
@@ -15,7 +18,7 @@ addButton.onclick = function(){
     // text
     let div1 = document.createElement("text");
     div1.setAttribute("class", "todoText");
-    div1.textContent = task ;
+    div1.textContent = task;
     // date N time
     let div2 = document.createElement("createdat");
     div2.setAttribute("class", "todoCreatedAt");
@@ -24,12 +27,37 @@ addButton.onclick = function(){
     let div3 = document.createElement("priority");
     div3.setAttribute("class", "todoPriority");
     div3.textContent = priority ;
+
+    let del = document.createElement("button");
+    del.textContent = "Delete";
+
+    let done = document.createElement("button");
+    done.textContent = "Done";
+
+    
     // stracture in html
     view.appendChild(container);
     container.appendChild(div3);
     container.appendChild(div2);
     container.appendChild(div1);
-}
+    container.appendChild(del);
+    container.appendChild(done);
+    // counter
+    let countdivs = document.getElementsByClassName("todoContainer").length;
+    let counter = document.querySelector("span");
+    counter.textContent = countdivs
+
+    myArray.push();
+
+
+};
+
+
+//when click sort
+sortButton.onclick = function(){
+      
+} 
+
 
 
 
